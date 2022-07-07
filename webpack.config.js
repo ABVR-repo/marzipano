c103fefaa24212c5e03c8c30215cb2d9f68b49d1
Cameron Abt <cameron.abt@atlasbayvr.com>
2022-07-07 15:11:42 -0400

    Finalized webpack setup for transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 /* global __dirname */ 		// eslint global declaration 
 
 const path = require('path');
-require('@babel/plugin-transform-runtime');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MARZIPANO_VERSION = require('./package.json').version;
@@ -10,32 +9,31 @@ const MARZIPANO_VERSION = require('./package.json').version;
 module.exports = env => { // eslint-disable-line no-unused-vars
 
 	const babelLoaderOptions = {
+		sourceType: 'unambiguous', // https://github.com/babel/babel/issues/12731#issuecomment-780184014
 		presets: [
-			['@babel/preset-env', {
-				modules: 'commonjs', // this is required otherwise it throws an error when loading the bundle; thinks its an esmodule?
+			['@babel/preset-env', { // https://babeljs.io/docs/en/babel-preset-env
+				modules: 'auto',
 				useBuiltIns: 'usage',
-				corejs: { version: '3.21' },
+				corejs: '3.23',
 				bugfixes: true,
 				shippedProposals: true,
 				targets: {
 					electron: '8.2.3',
 					browsers: [
-						'defaults', // base but doesn't include enough...
+						'defaults', // base doesn't include enough...
 						'not IE 11', // already in defaults, but want to call it out explicitly
 						'chrome 64', // from analytics
 						'safari 13', // from analytics
 						'edge 88', // from analytics
 						'firefox 78', // from analytics
-						// 'chrome 98', // testing to see latest
+						// 'chrome 98', // just for testing, to see latest
 					],
 				},
 				// debug: true,
 			}]
 		],
 		plugins: [
-			['@babel/plugin-transform-runtime', {
-				corejs: { version: '3' },
-			}],
+			// ['@babel/plugin-transform-runtime'] // don't use this plugin, the preset-env takes care of everything
 		]
 	};
 
@@ -70,8 +68,8 @@ module.exports = env => { // eslint-disable-line no-unused-vars
 			},
 		},
 		devtool: 'source-map',
-		mode: 'development', 
-		// mode: 'production',
+		// mode: 'development',
+		mode: 'production',
 
 		// PLUGINS
 		plugins: [
